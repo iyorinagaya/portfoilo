096f3469f89ffd299c4afac14055740b447e84cc
iyori <iyochi.nov8@gmail.com>
2021-06-29 14:08:46 +0900

    add profile/worksページの作成

---
+++ wp-content/themes/portfolio_nagaya/works.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Template Name: works
+*/
+?>
+<?php get_header(); ?>
+
+<main>
+  <section class="worksSingle">
+    <div class="wrapper">
+      <h2>- WORKS -</h2>
+      <p class="explanation">説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明</p>
+      <ul class="cards">
+        <li class="card">
+          <p class="work image1"><img src="<?php echo get_template_directory_uri(); ?>/assets/image/evgeniy-konev-M4DjXRLWdNA-unsplash.jpg" alt="beer1"></p>
+          <p class="text">説明説明説明説明説明説明説明説明説明説明説明説明</p>
+        </li>
+        <li class="card">
+          <p class="work image2"><img src="<?php echo get_template_directory_uri(); ?>/assets/image/timothy-dykes-Lq1rOaigDoY-unsplash.jpg" alt="beer2"></p>
+          <p class="text">説明説明説明説明説明説明説明説明説明説明説明説明</p>
+        </li>
+        <li class="card">
+          <p class="work image3"><img src="<?php echo get_template_directory_uri(); ?>/assets/image/jon-parry-C8eSYwQkwHw-unsplash.jpg" alt="beer3"></p>
+          <p class="text">説明説明説明説明説明説明説明説明説明説明説明説明</p>
+        </li>
+        <li class="card">
+          <p class="work image3"><img src="<?php echo get_template_directory_uri(); ?>s/assets/image/christin-hume-08tX2fsuSLg-unsplash.jpg" alt="beer4"></p>
+          <p class="text">説明説明説明説明説明説明説明説明説明説明説明説明</p>
+        </li>
+      </ul>
+    </div>
+  </section>
+</main>
+
+<?php get_footer(); ?>
