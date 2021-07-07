b8577b5621feca7cd61a9ae16f0bf61938b9eb06
iyori <iyochi.nov8@gmail.com>
2021-07-07 13:05:37 +0900

    fix cssの読み込み修正（header.phpから不要な読み込みタグを削除）

--- wp-content/themes/portfolio_nagaya/header.php
+++ wp-content/themes/portfolio_nagaya/header.php
@@ -5,8 +5,6 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php bloginfo('name'); ?></title>
-  <link rel="stylesheet" href="assets/css/style.css">
-  <link rel="stylesheet" href="assets/css/sanitize.css">
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;400&display=swap" rel="stylesheet">
   <?php wp_head(); ?>
