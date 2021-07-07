afaeeaf0ebe374809057aea75cc8cee5a575b046
iyori <iyochi.nov8@gmail.com>
2021-07-07 15:51:22 +0900

    fix functions.phpの命名をスネークに統一＆init発火の追加

--- wp-content/themes/portfolio_nagaya/functions.php
+++ wp-content/themes/portfolio_nagaya/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-function addScripts() {
+function add_scripts() {
   wp_deregister_script('jquery');
   wp_enqueue_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js', array(), "3.6.0", true);
   wp_enqueue_script('mainJs', get_theme_file_uri('/assets/js/index.js'), array('jquery'), true);
@@ -8,9 +8,9 @@ function addScripts() {
   wp_enqueue_style('mainStyle', get_theme_file_uri('/assets/css/style.css'), array());
 }
 
-add_action('wp_enqueue_scripts', 'addScripts');
+add_action('wp_enqueue_scripts', 'add_scripts');
 
-function addThemeSetup() {
+function add_theme_setup() {
   register_nav_menus(
     array(
       'global' => 'グローバル',
@@ -18,4 +18,15 @@ function addThemeSetup() {
   );
 }
 
-add_action('after_setup_theme', 'addThemeSetup');
+add_action('after_setup_theme', 'add_theme_setup');
+
+function hooks() {
+  add_action('wp_enqueue_scripts', 'add_scripts');
+  add_action('after_setup_theme', 'add_theme_setup');
+}
+
+function init() {
+  hooks();
+}
+
+init();
